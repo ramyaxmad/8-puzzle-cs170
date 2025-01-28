e5087b1c1d9bcad618ec5d4135c5dcdece5e480f
Ramya Madugula <ramyamadugula24@gmail.com>
2025-01-28 12:47:25 -0800

    got the initial state from user input

--- main.py
+++ main.py
@@ -2,7 +2,19 @@
 import heapq as min_heap_esque_queue
 import numpy as np
 
-depth0 = [[],[],[]]
+depth2 = [[1,2,3],[4,5,6],[0,7,8]]
+depth4 = [[1,2,3],[5,0,6],[4,7,8]]
+depth8 = [[1,3,6],[5,0,2],[4,7,8]]
+depth12 = [[1,3,6],[5,0,7],[4,8,2]]
+depth16 = [[1,6,7],[5,0,3],[4,8,2]]
+depth20 = [[7,1,2],[4,8,5],[6,3,0]]
+depth24 = [[0,7,2],[4,6,1],[3,5,8]]
+# depth4 = [[],[],[]]
+# depth4 = [[],[],[]]
+# depth4 = [[],[],[]]
+# depth4 = [[],[],[]]
+# depth4 = [[],[],[]]
+
 
 initial_State = []
 goal_State = [[1,2,3],
@@ -22,16 +34,26 @@ if puzzle == "2":
     entries += input("Enter second row: ") + " "
     entries += input("Enter third row: ")
 
-    print(entries)
+    #print(entries)
 
     entered = list(map(int, entries.split()))
     initial_State = np.array(entered).reshape(3,3)
-    print(initial_State)
+    #print(initial_State)
+
+depth_map = {
+        "2": depth2,
+        "4": depth4,
+        "8": depth8,
+        "12": depth12,
+        "16": depth16,
+        "20": depth20,
+        "24": depth24
+    }
 
 if puzzle == "1":
+    start = input("Enter # 0, 2, 4, 8, 12, 16, 20, or 24 for depth: \n")
+    initial_State = np.array(depth_map.get(start))
 
-
-
-
+print(initial_State)
 
 
