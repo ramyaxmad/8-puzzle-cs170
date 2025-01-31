fae24b85215c94343c071e9d3f0a5c5dcd2cea29
Ramya Madugula <ramyamadugula24@gmail.com>
2025-01-30 17:29:14 -0800

    trying binary search

--- main.py
+++ main.py
@@ -1,59 +1,184 @@
 #import TreeNode
-import heapq as min_heap_esque_queue
+import heapq
 import numpy as np
-
-depth2 = [[1,2,3],[4,5,6],[0,7,8]]
-depth4 = [[1,2,3],[5,0,6],[4,7,8]]
-depth8 = [[1,3,6],[5,0,2],[4,7,8]]
-depth12 = [[1,3,6],[5,0,7],[4,8,2]]
-depth16 = [[1,6,7],[5,0,3],[4,8,2]]
-depth20 = [[7,1,2],[4,8,5],[6,3,0]]
-depth24 = [[0,7,2],[4,6,1],[3,5,8]]
-# depth4 = [[],[],[]]
-# depth4 = [[],[],[]]
-# depth4 = [[],[],[]]
-# depth4 = [[],[],[]]
-# depth4 = [[],[],[]]
-
+import copy
 
 initial_State = []
 goal_State = [[1,2,3],
-                [4,5,6],
-                [7,8,0]]
-
-print("Welcome to Ramya's 8-Puzzle. ")
-puzzle = input("Type \'1\' to use a default puzzle, or \'2\' to create your own.\n")
-
-if puzzle == "2":
-    print("Enter your puzzle, using a zero to represent the blank.", end="") 
-    print("Please only enter valid 8-puzzles. Enter the puzzle demilimiting", end="")
-    print("the numbers with a space. Type RETURN only when finished.")
-
-    entries = ""
-    entries += input("Enter first row: ") + " "
-    entries += input("Enter second row: ") + " "
-    entries += input("Enter third row: ")
-
-    #print(entries)
-
-    entered = list(map(int, entries.split()))
-    initial_State = np.array(entered).reshape(3,3)
-    #print(initial_State)
-
-depth_map = {
-        "2": depth2,
-        "4": depth4,
-        "8": depth8,
-        "12": depth12,
-        "16": depth16,
-        "20": depth20,
-        "24": depth24
-    }
-
-if puzzle == "1":
-    start = input("Enter # 0, 2, 4, 8, 12, 16, 20, or 24 for depth: \n")
-    initial_State = np.array(depth_map.get(start))
-
-print(initial_State)
+              [4,5,6],
+              [7,8,0]]
+#Definition for a tree node.
 
 
+class Node:
+    def __init__(self, parent=None, board = None, cost = 0, depth = 0):
+        self.board = board
+        self.parent = parent
+        self.cost = cost
+        self.depth = depth
+    def children(self, node):
+        node.parent = self
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
+        childrn_nodes = []
+
+        # find the coordinates for 0, (i, j)
+        x = 0
+        y = 0
+        for i in range(3):
+            for j in range(3):
+                if self.board[i][j] == 0:
+                    x = i
+                    y = j
+        for d in directions:
+            new_i = i + d[0]
+            new_j = j + d[1]
+            #check if new_i, new_j is in the matrix
+            if 0 <= new_i < 3 and 0 <= new_j < 3:
+                #deep copy the board
+                new_board = copy.deepcopy(self.board)
+                #swap the 0 with the new_i, new_j
+                temp = new_board[i][j]
+                new_board[i][j] = new_board[new_i][new_j]
+                new_board[new_i][new_j] = temp
+                #make a node from the matrix
+                child_node = Node(None, new_board, 0, 0)
+                #append the new board to the result
+                childrn_nodes.append(child_node)
+        return childrn_nodes
+
+
+# directions = [(0,1), (0,-1), (1,0), (-1,0)]
+
+# find the coordinates for 0, (i, j)
+# for d in directions:
+# new_i = i + d[0]
+# new_j = j + d[1]
+# check if new_i, new_j is in the matrix
+# generate the new board
+#     deep copy the board
+#     swap the 0 with the new_i, new_j
+#     make a node from the matrix
+#     append the new board to the result
+
+def print_puzzle(puzzle):
+    print(np.array(puzzle) + "\n")
+
+def select_init_alg(puzzle):
+    alg = input("(1) Uniform Cost Search\n"
+                "(2) A* with Misplaced Tile heuristic\n"
+                "(3) A* with Manhattan Distance heuristic\n")
+    if alg == 1:
+        general_search(puzzle)
+    if alg == 2:
+        general_search(puzzle)
+    if alg == 3:
+        general_search(puzzle)
+
+def board_to_tuple(self): #converts board to tuple to put into dict()
+    return tuple(tuple(row) for row in self.board)
+
+# def uniform_cost_search(puzzle, heuristic):
+#     start_node = Node.Node(None, puzzle, 0, 0) #initial node, root
+#     priority_queue = [] #nodes sorted by cost?
+#     repeat_states = dict() #tracks visited states
+#     min_heap_esque_queue.heappush(priority_queue, start_node) #start_node pushed into priority_queue
+
+#     num_expanded_nodes = 0
+#     max_queue_size = 0
+#     repeat_states[start_node.board_to_tuple()] = "PARENT BOARD"
+
+#     stack_print = [] #boards stored in a stack
+
+#     while len(priority_queue) > 0:
+#         max_queue_size = max(len(priority_queue), max_queue_size)
+
+#         #node from queue checked
+#         node_from_queue = min_heap_esque_queue.heappop(priority_queue) #pop the first board in the queue
+#         repeat_states[node_from_queue.board_to_tuple()] = "this can be anything"
+
+def general_search(puzzle):
+    print("hi")
+    #make initial state a board, then make it a node, then push into queue 
+    repeat_states = dict()
+    priority_queue = []
+    curr_node= Node.Node(None, puzzle, 0, 0)
+    priority_queue.append(curr_node) #push into queue
+
+    num_expanded_nodes = 0
+    max_queue_size = 0
+    repeat_states[curr_node.board_to_tuple()] = "PARENT BOARD"
+    
+    #loop
+    while len(priority_queue):
+        #if theres nothing in the queue return failure
+        #pop first node in the queue
+        curr_node = priority_queue.pop(0) 
+        print_puzzle(curr_node.board)
+        #if popped node is goal state return the node 
+        if curr_node.board == goal_State:
+            return "goal state!"
+        #expand the children, put them into queue
+        children = curr_node.children() #is a list of nodes
+        #   iterate through list of nodes
+        #if its in repeat, dont add to queue
+        #its not in repeat, add to queue and add to repeat
+        for c in children:
+            if c.board_to_tuple() not in repeat_states:
+                priority_queue.append(c)
+                repeat_states[c.board_to_tuple()] = "EXPANDED"
+    return "failure"
+    #end
+    
+def main():
+    print("hi")
+    depth2 = [[1,2,3],[4,5,6],[0,7,8]]
+    depth4 = [[1,2,3],[5,0,6],[4,7,8]]
+    depth8 = [[1,3,6],[5,0,2],[4,7,8]]
+    depth12 = [[1,3,6],[5,0,7],[4,8,2]]
+    depth16 = [[1,6,7],[5,0,3],[4,8,2]]
+    depth20 = [[7,1,2],[4,8,5],[6,3,0]]
+    depth24 = [[0,7,2],[4,6,1],[3,5,8]]
+    # depth4 = [[],[],[]]
+    # depth4 = [[],[],[]]
+    # depth4 = [[],[],[]]
+    # depth4 = [[],[],[]]
+    # depth4 = [[],[],[]]
+
+
+    print("Welcome to Ramya's 8-Puzzle. ")
+    choose = input("Type \'1\' to use a default puzzle, or \'2\' to create your own.\n")
+
+    if choose == "2":
+        print("Enter your puzzle, using a zero to represent the blank.", end="") 
+        print("Please only enter valid 8-puzzles. Enter the puzzle demilimiting", end="")
+        print("the numbers with a space. Type RETURN only when finished.")
+
+        entries = ""
+        entries += input("Enter first row: ") + " "
+        entries += input("Enter second row: ") + " "
+        entries += input("Enter third row: ")
+
+        #print(entries)
+
+        entered = list(map(int, entries.split()))
+        initial_State = np.array(entered).reshape(3,3)
+        #print(initial_State)
+
+    depth_map = {
+            "2": depth2,
+            "4": depth4,
+            "8": depth8,
+            "12": depth12,
+            "16": depth16,
+            "20": depth20,
+            "24": depth24
+        }
+
+    if choose == "1":
+        start = input("Enter # 0, 2, 4, 8, 12, 16, 20, or 24 for depth: \n")
+        initial_State = np.array(depth_map.get(start))
+
+    print(initial_State)
+    select_init_alg(initial_State)
+
+main()
